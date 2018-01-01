4d57a07ddef4d74a5f90cca7416eea2b2dfc1e15
Kyle Wilson <ky.g.wilson@gmail.com>
2018-01-01 19:00:05 +0100

    adjusted db

--- data/createdb.py
+++ data/createdb.py
@@ -28,23 +28,26 @@ def create_tables(debug=False):
             db.add_field('races', 'wis_bonus', 'INTEGER', default=0, not_null=True)
             db.add_field('races', 'cha_bonus', 'INTEGER', default=0, not_null=True)
             db.add_field('races', 'languages', 'TEXT', default="Common", not_null=True)
-            db.add_field('races', 'trait_1', 'INTEGER')
-            db.add_field('races', 'trait_2', 'INTEGER')
-            db.add_field('races', 'trait_3', 'INTEGER')
-            db.add_field('races', 'trait_4', 'INTEGER')
-            db.add_field('races', 'trait_5', 'INTEGER')
-            db.add_field('races', 'trait_6', 'INTEGER')
-            db.add_field('races', 'trait_7', 'INTEGER')
-            db.add_field('races', 'trait_8', 'INTEGER')
+            db.add_field('races', 'trait_1', 'TEXT')
+            db.add_field('races', 'trait_2', 'TEXT')
+            db.add_field('races', 'trait_3', 'TEXT')
+            db.add_field('races', 'trait_4', 'TEXT')
+            db.add_field('races', 'trait_5', 'TEXT')
+            db.add_field('races', 'trait_6', 'TEXT')
+            db.add_field('races', 'trait_7', 'TEXT')
+            db.add_field('races', 'trait_8', 'TEXT')
 
             # Test Race Table
             if debug:
                 success = db.add_record('races',
                                         base_race='Dwarf',
                                         sub_race='Hill',
+                                        source='PHB',
                                         name='Hill Dwarf',
-                                        min_height=46,
-                                        max_height=57,
+                                        base_height=46,
+                                        height_mod='1d1',
+                                        base_weight=110,
+                                        weight_mod='1d1',
                                         speed=25,
                                         str_bonus=0,
                                         dex_bonus=0,
@@ -52,12 +55,13 @@ def create_tables(debug=False):
                                         int_bonus=0,
                                         wis_bonus=1,
                                         cha_bonus=0,
-                                        trait_1=6,
-                                        trait_2=15,
-                                        trait_3=16,
-                                        trait_4=41,
-                                        trait_5=36,
-                                        trait_6=17,
+                                        languages='Common',
+                                        trait_1='Darkvision',
+                                        trait_2='Dwarven Combat Training',
+                                        trait_3='Dwarven Resilience',
+                                        trait_4='Tool Proficiency',
+                                        trait_5='Stonecunning',
+                                        trait_6='Stout Resilience',
                                         trait_7=None,
                                         trait_8=None,)
 
@@ -81,6 +85,7 @@ def create_tables(debug=False):
             db.add_field('names', 'is_given', 'BOOLEAN', default=True, not_null=True)
             db.add_field('names', 'is_nickname', 'BOOLEAN', default=False, not_null=True)
             db.add_field('names', 'is_family', 'BOOLEAN', default=False, not_null=True)
+            db.add_field('names', 'translation', 'TEXT')
 
             if debug:
                 # Test Name Table
@@ -92,7 +97,9 @@ def create_tables(debug=False):
                                         is_child=False,
                                         is_given=True,
                                         is_nickname=False,
-                                        is_family=False)
+                                        is_family=False,
+                                        translation='Moonunit')
+
 
                 if success:
                     print("'Name' Table tested successfully.")
@@ -101,6 +108,59 @@ def create_tables(debug=False):
 
                 print('\n\n\n')
 
+
+        # Create Background Table
+        table = db.create_table('backgrounds')
+        if table:
+            db.add_field('backgrounds', 'background', 'TEXT', default="Acolyte", not_null=True)
+            db.add_field('backgrounds', 'source', 'TEXT', default='PHB', not_null=True)
+            db.add_field('backgrounds', 'skill1', 'TEXT')
+            db.add_field('backgrounds', 'skill2', 'TEXT')
+            db.add_field('backgrounds', 'languages', 'TEXT')
+            db.add_field('backgrounds', 'tool_proficiency', 'TEXT')
+            db.add_field('backgrounds', 'equipment', 'TEXT')
+
+            if debug:
+                # Test Name Table
+                success = db.add_record('backgrounds',
+                                        background='Hermit',
+                                        source='PHB',
+                                        skill1='Medicine',
+                                        skill2='Religion',
+                                        languages='One of your choice',
+                                        tool_proficiency='Herbalism Kit',
+                                        equipment='A scroll case stuffed full of notes from your studies or prayers, a winter blanket, a set of common clothes, a herbalism kit, and 5 gp')
+
+                if success:
+                    print("'Background' Table tested successfully.")
+                else:
+                    print("Failed to create new 'Background' record.")
+
+                print('\n\n\n')
+
+
+        # Create Background Characteristics Table
+        table = db.create_table('background_characteristics')
+        if table:
+            db.add_field('background_characteristics', 'background', 'TEXT', default="Acolyte", not_null=True)
+            db.add_field('background_characteristics', 'characteristic', 'TEXT', default='Trait', not_null=True)
+            db.add_field('background_characteristics', 'alignment', 'TEXT')
+            db.add_field('background_characteristics', 'description', 'TEXT')
+
+            if debug:
+                # Test Name Table
+                success = db.add_record('background_characteristics',
+                                        background='Acolyte',
+                                        characteristic='Type',
+                                        alignment=None,
+                                        description="I idolize a particular hero of my faith, and constantly refer to that person's deeds and example.",)
+
+                if success:
+                    print("'background_characteristics' Table tested successfully.")
+                else:
+                    print("Failed to create new 'background_characteristics' record.")
+
+                print('\n\n\n')
 
 
 def load_races():
@@ -128,19 +188,90 @@ def load_races():
                               int_bonus=race[13],
                               wis_bonus=race[14],
                               cha_bonus=race[15],
-                              trait_1=race[16],
-                              trait_2=race[17],
-                              trait_3=race[18],
-                              trait_4=race[19],
-                              trait_5=race[20],
-                              trait_6=race[21],
-                              trait_7=race[22],
-                              trait_8=race[23],)
+                              languages=race[16],
+                              trait_1=race[17],
+                              trait_2=race[18],
+                              trait_3=race[19],
+                              trait_4=race[20],
+                              trait_5=race[21],
+                              trait_6=race[22],
+                              trait_7=race[23],
+                              trait_8=race[24],)
                 print("{} created!".format(race[4]))
             except sqlite3.IntegrityError:
                 print("{} already exists.".format(race[4]))
 
 
+def load_names():
+    with open('csv_data/names.csv', 'r') as f:
+        csv_data = csv.reader(f)
+        print(next(csv_data))
+        names = [row for row in csv_data]
+
+    with SQLConnect('dnd.db') as db:
+        for name in names:
+            try:
+                db.add_record('names',
+                              name=name[1],
+                              race_id=name[2],
+                              gender=name[3],
+                              region=name[4],
+                              is_child=name[5],
+                              is_given=name[6],
+                              is_nickname=name[7],
+                              is_family=name[8],
+                              translation=name[9])
+
+                print("{} created!".format(name[1]))
+            except sqlite3.IntegrityError:
+                print("{} already exists.".format(name[1]))
+
+
+def load_backgrounds():
+    with open('csv_data/backgrounds.csv', 'r') as f:
+        csv_data = csv.reader(f)
+        print(next(csv_data))
+        backgrounds = [row for row in csv_data]
+
+    with SQLConnect('dnd.db') as db:
+        for background in backgrounds:
+            try:
+                db.add_record('backgrounds',
+                              background=background[1],
+                              source=background[2],
+                              skill1=background[3],
+                              skill2=background[4],
+                              languages=background[5],
+                              tool_proficiency=background[6],
+                              equipment=background[7])
+
+                print("{} created!".format(background[1]))
+            except sqlite3.IntegrityError:
+                print("{} already exists.".format(background[1]))
+
+def load_background_characteristics():
+    with open('csv_data/background_characteristics.csv', 'r') as f:
+        csv_data = csv.reader(f)
+        print(next(csv_data))
+        background_characterstics = [row for row in csv_data]
+
+    with SQLConnect('dnd.db') as db:
+        for background_characterstic in background_characterstics:
+            try:
+                db.add_record('background_characterstics',
+                              background=background_characterstic[1],
+                              characteristic=background_characterstic[2],
+                              alignment=background_characterstic[3],
+                              description=background_characterstic[4])
+
+                print("{} created!".format(background_characterstic[1]))
+            except sqlite3.IntegrityError:
+                print("{} already exists.".format(background_characterstic[1]))
+
+
 if __name__ == '__main__':
     create_tables()
-    load_races()
+    # load_races()
+    # load_names()
+    # load_backgrounds()
+    load_background_characteristics()
